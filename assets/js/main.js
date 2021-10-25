4b09a2873f6054bfcfa2af13627c234ac36393ae
MrGaggio <steruggiu@gmail.com>
2021-10-25 11:19:34 +0100

    Creato array di squadre

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,70 @@ Stampare a schermo la bici con peso minore. */
 
 //creato array di oggetti
 
-const bici = [
+/* const bike = [
     {
         nome: 'Atala',
-        peso: '5'
+        peso: 5
     },
 
     {
         nome: 'Bianchi',
-        peso: '10'
+        peso: 10
     },
 
     {
         nome: 'Canondale',
-        peso: '4'
+        peso: 4
     }
 
 ]
 
-
+/* 
 
 console.log(bici);
 
 const peso = document.querySelector([0], ".peso")
-console.log("peso");
+console.log("peso"); */
+
+/*
+ // prendere una bici di riferimento
+let lightestBike =  bicycles[0]
+
+ // cilclare tra le bici
+for (let i = 0; i < bicycles.length; i++) {
+    const bike = bicycles[i];
+    console.log(bike.weight < lightestBike.weight);
+    
+}
+ // verificare se la bici corrente è più leggera di quella selezionata
+ if (bike.weight < lightestBike.weight) {
+     lightestBike = bike
+ }; */
 
 
- 
+
+ const squadre = [
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0
+    },
+
+    {
+        nome: 'Cagliari',
+        punti: 0,
+        falli: 0
+    },
+
+    {
+        nome: 'Roma',
+        punti: 0,
+        falli: 0
+    },
+]

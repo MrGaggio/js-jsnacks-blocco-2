9a190c8d631435a4b62e44f479715fc2ccd603cf
MrGaggio <steruggiu@gmail.com>
2021-10-25 10:32:17 +0100

    Creato array di oggetti

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. */
+
+
+//creato array di oggetti
+
+const bici = [
+    {
+        nome: 'Atala',
+        peso: '5'
+    },
+
+    {
+        nome: 'Bianchi',
+        peso: '10'
+    },
+
+    {
+        nome: 'Canondale',
+        peso: '4'
+    }
+]

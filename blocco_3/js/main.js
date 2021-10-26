bfca5f25fae260342784a3d5388e7b6e44d14809
MrGaggio <steruggiu@gmail.com>
2021-10-26 11:19:11 +0100

    creata struttura snack 3

---
+++ blocco_3/js/main.js
@@ -0,0 +1,9 @@
+/*
+* Crea un array vuoto e chiedi all’utente un numero
+* da inserire nell’array. Continua a chiedere i numeri
+* all’utente e a inserirli nell’array fino a quando
+* a somma degli elementi è minore di 50.
+*/ (modificato) 
+
+
+// crea un array vuoto
